43d877a4b80442de91c20f86656ca44d0a4ce46c
Aashish518 <jadavaashish9@gmail.com.com>
2025-04-13 11:34:12 +0530

    update

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: 'src/index.js', // update if your entry file is different
+    input: 'src/main.jsx', // ✅ updated entry file
     output: {
         file: 'dist/bundle.js',
         format: 'esm',
